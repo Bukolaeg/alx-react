c66857e1979d82e1c47182a7251c386679f6da14
bukolaeg <bukolarongbe2015@gmail.com>
2024-05-27 15:18:21 +0100

    react  intro

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	  entry: './src/index.js',
+	  output: {
+		      filename: 'bundle.js',
+		      path: path.resolve(__dirname, 'dist'),
+		      clean: true,
+		    },
+	  mode: 'development',
+	  devServer: {
+		      static: {
+			            directory: path.join(__dirname, 'dist'),
+			          },
+		      compress: true,
+		      port: 9000,
+		      hot: true,
+		      open: true,
+		    },
+	  module: {
+		      rules: [
+			            {
+					            test: /\.js$/,
+					            exclude: /node_modules/,
+					            use: {
+							              loader: 'babel-loader',
+							              options: {
+									                  presets: ['@babel/preset-env', '@babel/preset-react'],
+									                },
+							            },
+					          },
+			            {
+					            test: /\.css$/,
+					            use: ['style-loader', 'css-loader'],
+					          },
+			            {
+					            test: /\.(png|jpg|gif)$/,
+					            type: 'asset/resource',
+					            use: [
+							              {
+									                  loader: 'image-webpack-loader',
+									                  options: {
+												                bypassOnDebug: true,
+												                disable: true,
+												              },
+									                },
+							            ],
+					          },
+			          ],
+		    },
+	  plugins: [
+		      new HtmlWebpackPlugin({
+			            template: './public/index.html',
+			            filename: 'index.html',
+			          }),
+		    ],
+};
+
--- 0x01-react_intro/task_5_dashboard/dist/bundle.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-console.log('Hello, Webpack!');
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUFBLE9BQU8sQ0FBQ0MsR0FBRyxDQUFDLGlCQUFpQixDQUFDLEMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90YXNrXzVfZGFzaGJvYXJkLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdIZWxsbywgV2VicGFjayEnKTtcbiJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sInNvdXJjZVJvb3QiOiIifQ==
